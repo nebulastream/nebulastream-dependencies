216bb6535528055b231369e95a75aa57cf33a0af
Philipp Grulich <philippgrulich@hotmail.de>
2021-05-25 11:14:18 +0200

    try vcpkg

--- x64-osx-nes.cmake
+++ x64-osx-nes.cmake
@@ -1,7 +1,10 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
+set(VCPKG_OSX_ARCHITECTURES x86_64)
+
 
 if (PORT STREQUAL paho-mqtt)
 	set(VCPKG_LIBRARY_LINKAGE dynamic)
@@ -16,11 +19,9 @@ if (PORT STREQUAL paho-mqttpp3)
 endif()
 
 
-
 if (PORT STREQUAL paho-mqttpp3)
 	set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
-
 
 if (PORT STREQUAL llvm)
 	set(VCPKG_BUILD_TYPE release)
