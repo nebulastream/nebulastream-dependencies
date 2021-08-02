e34c9ceadd6ce968c6680dde6780059e6ab84763
Ventura Del Monte <venturadelmonte@gmail.com>
2021-08-02 22:01:54 +0200

    Update macos deps

--- build-osx-x64.sh
+++ build-osx-x64.sh
@@ -8,17 +8,16 @@ outputFileName="nes-dependencies-$version-$VCPKG_DEFAULT_TRIPLET"
 outputDir="."
 libs=(
 # The LLVM Compiler Infrastructure.
-"llvm[core,clang,target-x86]"
-"protobuf"
-# Z3 is a theorem prover from Microsoft Research.
-"z3"
-# Boost
-"boost-system"
-"boost-process"
-"boost-thread"
-"boost-program-options"
-"boost-filesystem"
-"boost-chrono"
+# "llvm[core,clang,target-x86]"
+# # Z3 is a theorem prover from Microsoft Research.
+# "z3"
+# # Boost
+# "boost-system"
+# "boost-process"
+# "boost-thread"
+# "boost-program-options"
+# "boost-filesystem"
+# "boost-chrono"
 # The ZeroMQ lightweight messaging kernel 
 "zeromq"
 # lightweight messaging kernel, C++ bindings
@@ -26,7 +25,7 @@ libs=(
 # Apache log4cxx is a logging framework
 "log4cxx"
 # An RPC library and framework
-"grpc[core]"
+"grpc"
 # An open-source C++ library developed and used at Facebook. 
 "folly"
 # C++11 JSON, REST, and OAuth library The C++ REST SDK is a Microsoft
@@ -36,17 +35,17 @@ libs=(
 # Formatting library for C++. It can be used as a safe alternative to printf or as a fast alternative to IOStreams.
 "fmt"
 # Paho project provides open-source client implementations of MQTT and MQTT-SN messaging protocols 
-"paho-mqtt"
-# Paho project provides open-source C++ wrapper for Paho C library
-"paho-mqttpp3" 
-# kafka lib
-"cppkafka"
-# jemalloc is a general purpose malloc(3) implementation that emphasizes fragmentation avoidance and scalable concurrency support
+# "paho-mqtt"
+# # Paho project provides open-source C++ wrapper for Paho C library
+# "paho-mqttpp3" 
+# # kafka lib
+# "cppkafka"
+# # jemalloc is a general purpose malloc(3) implementation that emphasizes fragmentation avoidance and scalable concurrency support
 "jemalloc"
-# An open source, portable, easy to use, readable and flexible SSL library
-"mbedtls"
-# open62541 is an open source C (C99) implementation of OPC UA licensed under the Mozilla Public License v2.0.
-"open62541"
+# # An open source, portable, easy to use, readable and flexible SSL library
+# "mbedtls"
+# # open62541 is an open source C (C99) implementation of OPC UA licensed under the Mozilla Public License v2.0.
+# "open62541"
 # libsodium is necessary as 3rd-party library
 "libsodium"
 # template library for linear algebra: matrices, vectors, numerical solvers, and related algorithms
