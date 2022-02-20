78bc79dbe534b23811a122330185ffdd088ffc32
digiou <digitalbckp@gmail.com>
2022-02-20 23:05:29 +0100

    [#2556] Add musl-based alpine 3.15 build

---
+++ custom-triplets/x64-linux-musl-nes.cmake
@@ -0,0 +1,29 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+if(NOT CMAKE_HOST_SYSTEM_PROCESSOR)
+    execute_process(COMMAND "uname" "-m" OUTPUT_VARIABLE CMAKE_HOST_SYSTEM_PROCESSOR OUTPUT_STRIP_TRAILING_WHITESPACE)
+endif()
+
+if (PORT STREQUAL paho-mqtt)
+	set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+if (PORT STREQUAL eclipse-paho-mqtt-c)
+	set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+if (PORT STREQUAL paho-mqttpp3)
+	set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+if (PORT STREQUAL llvm)
+	set(VCPKG_BUILD_TYPE release)
+endif()
+
+if (PORT STREQUAL open62541)
+    set(VCPKG_POLICY_ALLOW_RESTRICTED_HEADERS enabled)
+endif()
