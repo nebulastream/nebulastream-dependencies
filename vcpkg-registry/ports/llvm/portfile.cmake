0b14855faffeee0aa8fe197c00caf17633926f26
Philipp Grulich <philippgrulich@hotmail.de>
2022-04-20 11:28:09 +0200

    add libunwind

--- vcpkg-registry/ports/llvm/portfile.cmake
+++ vcpkg-registry/ports/llvm/portfile.cmake
@@ -164,6 +164,8 @@ if("pstl" IN_LIST FEATURES)
     list(APPEND LLVM_ENABLE_PROJECTS "pstl")
 endif()
 
+list(APPEND LLVM_ENABLE_PROJECTS "libunwind")
+
 set(LLVM_ENABLE_RUNTIMES)
 if("libcxx" IN_LIST FEATURES)
     if(VCPKG_TARGET_IS_WINDOWS)
