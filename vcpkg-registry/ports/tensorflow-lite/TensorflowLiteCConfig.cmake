8901b4e8f5042c40b72544e5a302046c5c37775b
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2022-10-30 10:27:23 +0100

    #19: updated tensorflow custom cmake file

--- vcpkg-registry/ports/tensorflow-lite/TensorflowLiteCConfig.cmake
+++ vcpkg-registry/ports/tensorflow-lite/TensorflowLiteCConfig.cmake
@@ -6,10 +6,21 @@ set(tensorflowlite_c_INCLUDE_DIRS
         )
 
 add_library(tensorflowlite_c::tensorflowlite_c SHARED IMPORTED)
-set_target_properties(tensorflowlite_c::tensorflowlite_c
-        PROPERTIES
-        IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/../../lib/libtensorflowlite_c.so
-        INTERFACE_INCLUDE_DIRECTORIES "${tensorflowlite_c_INCLUDE_DIR}"
-        )
+
+if (APPLE)
+    set_target_properties(tensorflowlite_c::tensorflowlite_c
+            PROPERTIES
+            IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/../../lib/libtensorflowlite_c.dylib
+            INTERFACE_INCLUDE_DIRECTORIES "${tensorflowlite_c_INCLUDE_DIR}"
+            )
+elseif (UNIX AND NOT APPLE)
+    set_target_properties(tensorflowlite_c::tensorflowlite_c
+            PROPERTIES
+            IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/../../lib/libtensorflowlite_c.so
+            INTERFACE_INCLUDE_DIRECTORIES "${tensorflowlite_c_INCLUDE_DIR}"
+            )
+else()
+    message(FATAL_ERROR "Unsupported platform for tensorflow")
+endif ()
 
 set(tensorflowlite_c_FOUND TRUE)
