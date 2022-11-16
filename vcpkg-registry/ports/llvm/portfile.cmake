29eccbffb33281ff7ca5c4bd5a403e4c3ae99def
Dwi Nugroho <dwiprasetyo.an@gmail.com>
2022-11-16 21:56:37 +0100

    #16: revert llvm version

--- vcpkg-registry/ports/llvm/portfile.cmake
+++ vcpkg-registry/ports/llvm/portfile.cmake
@@ -1,4 +1,4 @@
-set(LLVM_VERSION "14.0.6")
+set(LLVM_VERSION "14.0.3")
 
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
@@ -6,7 +6,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO llvm/llvm-project
     REF llvmorg-${LLVM_VERSION}
-    SHA512 d64f97754c24f32deb5f284ebbd486b3a467978b7463d622f50d5237fff91108616137b4394f1d1ce836efa59bf7bec675b6dee257a79b241c15be52d4697460
+    SHA512 511e93fd9b1c414c38fe9e2649679ac0b16cb04f7f7838569d187b04c542a185e364d6db73e96465026e3b2533649eb75ac95507d12514af32b28bdfb66f2646
     HEAD_REF master
     PATCHES
         0002-fix-install-paths.patch    # This patch fixes paths in ClangConfig.cmake, LLVMConfig.cmake, LLDConfig.cmake etc.
