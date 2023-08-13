1bbd20cd8ab474debfe91330d55c8672eb2fe78f
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2023-08-13 17:24:07 +0200

    CXX 20 Fixes / Abseil / Cleanup

--- vcpkg-registry/ports/onnxruntime/portfile.cmake
+++ vcpkg-registry/ports/onnxruntime/portfile.cmake
@@ -9,15 +9,16 @@ vcpkg_from_github(
     SHA512 deecbaa09aeb2d03bf5a2c075531862482c44a6ba075dce94e87864659bab0b77a7a91dd9f4713d47efe260748deaf7cc55e16a4354f531c59704bed19a525e2
     HEAD_REF master
     PATCHES
+    cxx-version.patch
     fix-deps2.patch
-    #fix-deps3.patch
     fix-deps4.patch
     fix-deps5.patch
     fix-deps6.patch
+    ignore-deprecated-warnings-for-cxx20.patch
+    CXX20-Error-Comparision-Is-Ambigous-fix.patch
+    cxx20-fix-u8-conversion-to-char.patch
+    cxx20-fix-template-inheritance.patch
     fbs-verify.patch
-    #    fix-build-issues.patch # Remove this patch in the next update
-    #   export-target-in-static-build.patch  # Remove this patch in the next update
-        # flatbuffer-fix.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
@@ -31,70 +32,9 @@ else()
 endif()
 string(COMPARE EQUAL "${VCPKG_HOST_TRIPLET}" "${VCPKG_TARGET_TRIPLET}" BUILD_HOST)
 
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-        gpu onnxruntime_USE_CUDA
-)
-
-file(TO_CMAKE_PATH "$ENV{CUDA_PATH}/bin/nvcc${VCPKG_HOST_EXECUTABLE_SUFFIX}" NVCC_PATH)
-
-#set(EXTRA_OPTIONS )
-#if ("gpu" IN_LIST FEATURES)
-#    list(APPEND EXTRA_OPTIONS
-#        "-Donnxruntime_CUDA_HOME=$ENV{CUDA_PATH}"
-#        "-Donnxruntime_CUDNN_HOME=$ENV{CUDA_PATH}"
-#        "-DCMAKE_CUDA_COMPILER=${NVCC_PATH}"
-#    )
-#endif()
-
 vcpkg_find_acquire_program(PYTHON3)
 get_filename_component(PYTHON_DIR "${PYTHON3}" PATH)
 vcpkg_add_to_path(PREPEND "${PYTHON_DIR}")
-
-#        -Donnxruntime_BUILD_SHARED_LIB=${BUILD_SHARED}
-#        -Donnxruntime_BUILD_FOR_NATIVE_MACHINE=${BUILD_HOST}
-#        -Donnxruntime_CROSS_COMPILING=${CROSS_BUILD}
-#        -DCMAKE_INSTALL_INCLUDEDIR=include
-#        -Donnxruntime_RUN_ONNX_TESTS=OFF
-#        -Donnxruntime_ENABLE_CUDA_PROFILING=OFF
-#        -Donnxruntime_ENABLE_CUDA_LINE_NUMBER_INFO=OFF
-#        -Donnxruntime_GENERATE_TEST_REPORTS=OFF
-#        -Donnxruntime_ENABLE_STATIC_ANALYSIS=OFF
-#        -Donnxruntime_ENABLE_PYTHON=OFF
-#        -Donnxruntime_ENABLE_MEMLEAK_CHECKER=OFF
-#        -Donnxruntime_USE_NNAPI_BUILTIN=OFF
-#        -Donnxruntime_DEV_MODE=OFF
-#        -Donnxruntime_BUILD_UNIT_TESTS=OFF
-#        -Donnxruntime_BUILD_CSHARP=OFF
-#        -Donnxruntime_BUILD_OBJC=OFF
-#        -Donnxruntime_USE_PREINSTALLED_EIGEN=ON
-#        -Donnxruntime_BUILD_BENCHMARKS=OFF
-#        -Donnxruntime_USE_LLVM=OFF
-#        -Donnxruntime_USE_AVX=OFF
-#        -Donnxruntime_USE_AVX2=OFF
-#        -Donnxruntime_USE_AVX512=OFF
-#        -Donnxruntime_USE_OPENMP=OFF
-#        -Donnxruntime_BUILD_APPLE_FRAMEWORK=OFF
-#        -Donnxruntime_ENABLE_MICROSOFT_INTERNAL=OFF
-#        -Donnxruntime_USE_TENSORRT=OFF
-#        -Donnxruntime_ENABLE_LTO=ON
-#        -Donnxruntime_DEBUG_NODE_INPUTS_OUTPUTS=OFF
-#        -Donnxruntime_USE_ROCM=OFF # AMD GPU SUPPORT
-#        -Donnxruntime_PREFER_SYSTEM_LIB=ON
-#        -Donnxruntime_MINIMAL_BUILD=OFF
-#        -Donnxruntime_EXTENDED_MINIMAL_BUILD=OFF
-#        -Donnxruntime_MINIMAL_BUILD_CUSTOM_OPS=OFF
-#        -Donnxruntime_DISABLE_EXTERNAL_INITIALIZERS=OFF
-#        -Donnxruntime_USE_VALGRIND=OFF
-#        -Donnxruntime_RUN_MODELTEST_IN_DEBUG_MODE=OFF
-#        -Donnxruntime_FUZZ_TEST=OFF
-#        -Donnxruntime_USE_NCCL=OFF
-#        -Donnxruntime_USE_MPI=OFF
-#        -Donnxruntime_ENABLE_BITCODE=OFF
-#        -Donnxruntime_BUILD_OPSCHEMA_LIB=OFF
-#        -Donnxruntime_USE_EXTENSIONS=OFF
-#        ${FEATURE_OPTIONS}
-#        ${EXTRA_OPTIONS}
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/cmake"
