ab45b1e125831ddfaea3bb9541484db6398c6c70
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2023-09-04 19:06:49 +0200

    test dwarf debug version = 4

--- custom-triplets/x64-linux-nes.cmake
+++ custom-triplets/x64-linux-nes.cmake
@@ -2,7 +2,8 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CXX_FLAGS "-std=c++20")
-set(VCPKG_C_FLAGS "")
+set(VCPKG_CXX_FLAGS "-fdebug-default-version=4")
+set(VCPKG_C_FLAGS "-fdebug-default-version=4")
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
